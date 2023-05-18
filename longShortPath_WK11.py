8f7962bb5003fb16b3fa3e338ad378397665848e
emmanestallo <erestallo@up.edu.ph>
2023-05-18 22:13:02 +0800

    activities

--- longShortPath_WK11.py
+++ longShortPath_WK11.py
@@ -3,13 +3,14 @@ v,e,c = [int(i) for i in input().split(' ')]
 inf = 100000000
 
 final_weights = [inf for i in range(v)]
-final_weights[c-1] = 0
 
-current_weights = final_weights 
+dist = [inf]*v 
+dist[c] = 0  
 
-edgeList = [] 
+isProbed = [False]*v 
+isProbed[c] = True 
+
+adjMat = [[inf]*v]*v  
 
 for pairs in range(e): 
-    edgeList.append([int(j) for j in input().split(' ')] + [False])
-
-print(edgeList)
+    [node1,node2,weight] = [int(j) for j in range(3)]
